6b5458ac54ea51273930609a9336e802184be21b
martucciv <vmartucci@knights.ucf.edu>
2021-06-03 16:12:32 -0400

    Updated Code

---
+++ src/main/java/ex02/CountingNum.java
@@ -0,0 +1,34 @@
+package ex02;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solutions
+ *  Copyright 2021 Veronica Martucci
+ */
+
+import java.util.Scanner;
+
+public class CountingNum {
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args){
+        CountingNum count = new CountingNum();
+        String string = count.readInput();
+        int length = count.getLength(string);
+        count.printOutput(string, length);
+    }
+
+    public String readInput(){
+        System.out.print("What is the input string? ");
+        String string = input.nextLine();
+        return string;
+    }
+
+    public int getLength(String string){
+        int length = string.length();
+        return length;
+    }
+
+    public void printOutput(String string, int length){
+        System.out.printf("%s has %d characters.", string, length);
+
+    }
+}
